538299443ca282faf41033f6c9273e8936646bf5
Shayane Umar <shayane.umar@gmail.com>
2018-07-31 11:18:27 +0530

    Shore polymer changes

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -12,6 +12,20 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    files: [
+      { pattern: '../node_modules/@mitel/shor-webcomponentsjs/webcomponents-lite.min.js', included: true, watched: false },
+      { pattern: '../node_modules/@mitel/shor-polymer/polymer.html', included: true, watched: false },
+      // { pattern: '../node_modules/@mitel/shor-textarea/shor-textarea.html', included: true, watched: false },
+      { pattern: '../node_modules/@mitel/shor-textfield/shor-textfield.html', included: true, watched: false },
+      // { pattern: '../node_modules/@mitel/shor-button/shor-button.html', included: true, watched: false },
+      // { pattern: '../node_modules/@mitel/shor-toggle/shor-toggle.html', included: true, watched: false },
+      // { pattern: '../node_modules/@mitel/shor-avatar/shor-avatar.html', included: true, watched: false },
+      // { pattern: '../node_modules/@mitel/shor-toolbar/shor-toolbar.html', included: true, watched: false },
+      // { pattern: '../node_modules/@mitel/shor-contextmenu/shor-contextmenu.html', included: true, watched: false }
+    ],
+    preprocessors: {
+      // './src/test.ts': ['@angular-devkit/build-angular']
+    },
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
